78fbcc6df399e724b286e6a194c2fad51ec7d5ac
Hiram <hiramtan@live.com>
2016-06-23 11:54:50 +0800

    泛型资源加载

--- unity/Assets/Script/Core/ResourceMgr.cs
+++ unity/Assets/Script/Core/ResourceMgr.cs
@@ -29,7 +29,7 @@ public class ResourceMgr
     /// <param name="paramFolder"></param>
     /// <param name="paramName"></param>
     /// <returns></returns>
-    public Object GetObj(string param)
+    public T GetObj<T>(string param) where T:UnityEngine.Object
     {
         int tempIndex = param.LastIndexOf("#");
         string paramFolder = param.Substring(0, tempIndex);
@@ -37,7 +37,7 @@ public class ResourceMgr
         string paramName = param.Substring(tempIndex + 1);
 #if UNITY_EDITOR
         var temp = "Assets/" + paramFolder + "/" + paramName;
-        return AssetDatabase.LoadAssetAtPath(temp, typeof(Object));
+        return AssetDatabase.LoadAssetAtPath<T>(temp);
 #endif
         paramFolder = paramFolder.Replace("/", "");
         paramFolder = paramFolder.ToLower();
@@ -45,14 +45,14 @@ public class ResourceMgr
         paramName = paramName.ToLower();
         AssetBundle tempAB = AssetBundleMgr.GetAssetBundle(paramFolder);
         if (tempAB != null && tempAB.Contains(paramName))
-            return tempAB.LoadAsset(paramName);
+            return tempAB.LoadAsset<T>(paramName);
         Debug.LogError("the things you want to load is null");
         return null;
     }
 
     public GameObject GetGameObject(string param)
     {
-        var temp = GetObj(param);
+        Object temp = GetObj<Object>(param);
         if (temp != null)
             return (GameObject)MonoBehaviour.Instantiate(temp);
         return null;
